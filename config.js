94ed4f7733c55731e91a4f0360b14558b9645fde
Alejandro Matamala <matamala@material.cl>
2016-10-31 16:06:29 -0400

    first

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key:         'xdQY4MhsWomTHCIEAluWDGkR6',
+  consumer_secret:      '40WduOMhbzgd7ZE7Fko001Y1zkuUgFH83SBFxsoyrMsjrt47sH',
+  access_token:         '787845610938441728-YbileSC8ouV7JLYda7B0TzD7wfzk7M0',
+  access_token_secret:  'xJhvWET2ucnVzwNmj6rYqicyfBcl3PD3aBVkvzaHC7lui',
+  
+}
---
+++ sketch/sketch.pde
@@ -0,0 +1,8 @@
+void setup() {
+  size(300,300);
+  background(255);
+  line(random(width),random(height),random(width),50);
+  save("output.png");
+  exit();
+
+}
