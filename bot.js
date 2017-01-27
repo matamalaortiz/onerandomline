6592a10ed97ba5b4e8c515818ed7e0b33943ed11
Alejandro Matamala <alejandromatamala@Alejandros-MBP.home>
2017-01-27 10:34:08 -0500

    changes color

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ setInterval(tweetIt, 1000*60*60);
 function tweetIt() {
   var cmd = 'processing-java --sketch=`pwd`/sketch --run';
   exec(cmd, processing);
-  
+
   function processing() {
   	var filename = 'sketch/output.png';
   	var params = {
@@ -28,12 +28,16 @@ function tweetIt() {
     function uploaded(err, data, response) {
       var id = data.media_id_string;
 	  var tweet = {
-	    // status: '#codingrainbow live from node.js',
+	    status: 'A new line',
 	    media_ids: [id]
 	  }
       T.post('statuses/update', tweet, tweeted);
 
     }
+
+
+
+
 
 	function tweeted(err, data, response) {
 	  if (err) {
--- sketch/sketch.pde
+++ sketch/sketch.pde
@@ -1,6 +1,7 @@
 void setup() {
   size(300,300);
-  background(255);
+  background(0);
+  stroke(255);
   line(random(width),random(height),random(width),50);
   save("output.png");
   exit();
